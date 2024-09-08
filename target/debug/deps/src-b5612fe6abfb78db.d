80de5740eca0a0e514bb9335fda84ef72c0a8d85
malachi-crain <malachicrain@gmail.com>
2024-09-08 13:02:26 -0500

    Set up rust

---
+++ target/debug/deps/src-b5612fe6abfb78db.d
@@ -0,0 +1,5 @@
+/Users/malachicrain/Documents/College/CS-403/403-lox-interpreter/target/debug/deps/src-b5612fe6abfb78db: src/main.rs
+
+/Users/malachicrain/Documents/College/CS-403/403-lox-interpreter/target/debug/deps/src-b5612fe6abfb78db.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/src.d
@@ -0,0 +1,1 @@
+/Users/malachicrain/Documents/College/CS-403/403-lox-interpreter/target/debug/src: /Users/malachicrain/Documents/College/CS-403/403-lox-interpreter/src/main.rs
