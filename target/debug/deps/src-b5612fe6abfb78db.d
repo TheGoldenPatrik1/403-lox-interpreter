6fcedda90eed967b8d6e80c620dde8001d4ded80
malachi-crain <malachicrain@gmail.com>
2024-09-08 13:51:22 -0500

    Untrack files in .gitignore

--- target/debug/deps/src-b5612fe6abfb78db.d
+++
@@ -1,5 +0,0 @@
-/Users/malachicrain/Documents/College/CS-403/403-lox-interpreter/target/debug/deps/src-b5612fe6abfb78db: src/main.rs
-
-/Users/malachicrain/Documents/College/CS-403/403-lox-interpreter/target/debug/deps/src-b5612fe6abfb78db.d: src/main.rs
-
-src/main.rs:
