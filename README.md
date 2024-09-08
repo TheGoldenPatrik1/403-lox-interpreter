26fa1258303baf30ad08c3dcb5c699399eabc9a8
malachi-crain <malachicrain@gmail.com>
2024-09-08 13:43:54 -0500

    Basic IO, readme, & gitignore

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 403-lox-interpreter
+
+This is a basic interpreter for the [Lox](https://craftinginterpreters.com/the-lox-language.html) programming language, implemented using [Rust](https://www.rust-lang.org/).
+
+## Steps to Run
+
+1. Install [Rustup](https://www.rust-lang.org/learn/get-started), which includes Rust and Cargo.
+2. Execute `cargo run`.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+use std::env;
+use std::io;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 2 {
+        eprintln!("Usage: cargo run <file_path>");
+        std::process::exit(1);
+    } else if args.len() == 2 {
+        run_file(&args[1]);
+    } else {
+        run_prompt();
+    }
+}
+
+fn run_file(file_path: &str) {
+    let path = Path::new(file_path);
+    let mut file = match File::open(&path) {
+        Ok(file) => file,
+        Err(err) => {
+            eprintln!("Error: Could not open file '{}'. {}", file_path, err);
+            std::process::exit(1);
+        }
+    };
+
+    let mut contents = String::new();
+    if let Err(err) = file.read_to_string(&mut contents) {
+        eprintln!("Error: Could not read from file '{}'. {}", file_path, err);
+        std::process::exit(1);
+    }
+
+    run(&contents);
+}
+
+fn run_prompt() {
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        
+        let mut input = String::new();
+        let bytes_read = io::stdin().read_line(&mut input);
+        match bytes_read {
+            Ok(0) => {
+                break;
+            }
+            Ok(_) => {
+                run(&input.trim());
+            }
+            Err(err) => {
+                eprintln!("Error reading input: {}", err);
+                break;
+            }
+        }
+    }
+}
+
+fn run(source: &str) {
+    println!("Running source:\n{}", source);
 }
