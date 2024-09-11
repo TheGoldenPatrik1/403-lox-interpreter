5054177fad1f50e20c89284c7c33aa35f5c980ad
sawyerke <sawyerke1211@gmail.com>
2024-09-11 12:42:23 -0500

    Finished Scanner Chapter 4

--- src/Scanner.rs
+++ src/Scanner.rs
@@ -1,4 +1,7 @@
-mod TokenType
+use std::collections::HashMap;
+
+mod TokenType;
+mod main;
 
 struct Scanner {
     source: String,
@@ -6,17 +9,38 @@ struct Scanner {
     start: i32,
     current: i32,
     line: i32,
+    keywords: HashMap<String, TokenType>,
 }
 
 impl Scanner {
     // Constructor
     fn new(source: String) -> Scanner {
+
+        let mut keywords = HashMap::new();
+        keywords.insert("and".to_string(), TokenType::AND);
+        keywords.insert("class".to_string(), TokenType::CLASS);
+        keywords.insert("else".to_string(), TokenType::ELSE);
+        keywords.insert("false".to_string(), TokenType::FALSE);
+        keywords.insert("for".to_string(), TokenType::FOR);
+        keywords.insert("fun".to_string(), TokenType::FUN);
+        keywords.insert("if".to_string(), TokenType::IF);
+        keywords.insert("nil".to_string(), TokenType::NIL);
+        keywords.insert("or".to_string(), TokenType::OR);
+        keywords.insert("print".to_string(), TokenType::PRINT);
+        keywords.insert("return".to_string(), TokenType::RETURN);
+        keywords.insert("super".to_string(), TokenType::SUPER);
+        keywords.insert("this".to_string(), TokenType::THIS);
+        keywords.insert("true".to_string(), TokenType::TRUE);
+        keywords.insert("var".to_string(), TokenType::VAR);
+        keywords.insert("while".to_string(), TokenType::WHILE);
+
         Scanner{
             source,
             tokens: Vec::new(),
             start: 0,
             current: 0,
             line: 1,
+            keywords,
         }
     }
 
@@ -69,8 +93,83 @@ impl Scanner {
             } else {
                 TokenType::GREATER
             }),
-            _   => panic!("Unexpected character."),
+            '/' => {
+                if self.match_char('/') {
+                    while self.peek() != '\n' && !self.is_at_end() {
+                        self.advance();
+                    }
+                } else {
+                    self.add_token(TokenType::SLASH);
+                }
+            },
+            ' ' | '\r' | '\t' => {
+
+            },
+            '\n' => {
+                self.line += 1;
+            },
+            '"' => self.string(),
+            _   => {
+                if self.is_digit(c) {
+                    number();
+                } else if is_alpha(c) {
+                    identifier();
+                } else {
+                    main.error(self.line, "Unexpected character.");
+                }
+            },
         }
+    }
+
+    fn identifier(&mut self) {
+        while self.is_alpha_numeric(self.peek()) {
+            self.advance()
+        }
+        let text = &self.source[self.start..self.current]; // Get substring
+        let token_type = self.keywords.get(text).unwrap_or(&TokenType::IDENTIFIER); // Check if it's in the keywords, default to IDENTIFIER
+        self.add_token(*token_type);
+    }
+
+    fn number(&mut self) {
+
+        while self.is_digit(self.peek()) {
+            self.advance()
+        }
+
+        if self.peek() == '.' && self.is_digit(self.peek_next()) {
+            // Consume the "."
+            self.advance();
+
+            // Consume the digits for the fractional part
+            while self.is_digit(self.peek()) {
+                self.advance();
+            }
+        }
+
+        let value: f64 = self.source[self.start..self.current].parse().expect("Failed to parse number");
+
+        self.add_token(TokenType::Number, Some(value));
+    }
+
+    fn string(&mut self) {
+        while self.peek() != '"' && !self.is_at_end() {
+            if self.peek() == '\n' {
+                self.line += 1;
+            }
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            self.error(self.line, "Unterminated string.");
+            return;
+        }
+
+        // Consume the closing "
+        self.advance();
+
+        // Get the string content by trimming the surrounding quotes
+        let value = &self.source[self.start + 1..self.current - 1];
+        self.add_token(TokenType::String(value.to_string()));
     }
 
     fn match_char(&mut self, expected: char) -> bool {
@@ -82,6 +181,36 @@ impl Scanner {
         }
         self.current += 1;
         true
+    }
+
+    fn peek(&self) -> char {
+        if self.is_at_end() {
+            '\0'
+        } else {
+            self.source.chars().nth(self.current).unwrap();
+        }
+    }
+
+    fn peek_next(&self) -> char {
+        if self.current + 1 >= self.source.len() {
+            '\0'
+        } else {
+            self.source.chars().nth(self.current + 1).unwrap_or('\0')
+        }
+    }
+
+    fn is_alpha(c: char) -> bool {
+        (c >= 'a' && c <= 'z') ||
+        (c >= 'A' && c <= 'Z') ||
+        c == '_'
+    }
+
+    fn is_alpha_numeric(&self, c: char) ->bool {
+        self.is_alpha(c) || self.is_digit(c)
+    }
+
+    fn is_digit(c: char) -> bool {
+        c >= '0' && c <= '9'
     }
 
     fn is_at_end(&self) -> bool {
